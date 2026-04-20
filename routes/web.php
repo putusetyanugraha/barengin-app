f250679392362ea3c7fc378f8e84b41bf7b4a8d0
Edwin Hendly <edwinhendly17@gmail.com>
2026-04-20 21:41:10 +0700

    add: auth controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function(){
     return inertia('Home/Index');
 });
+
+Route::get('/login', [AuthController::class, 'login']);
